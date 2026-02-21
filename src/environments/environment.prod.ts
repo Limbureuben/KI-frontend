ac82e0585849495b69a2c7922fb26cd91afe7be6
HeemTempo <heemsterling1706@gmail.com>
2026-02-21 15:00:58 +0300

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // Your live Dokploy Django URL
+
   apiUrl: 'http://95.111.247.129:8000', 
-  // Your live GraphQL endpoint
   graphqlUrl: 'http://95.111.247.129:8000' 
 };
