67a638b94e787dea78e97a1a2b3a70cf6f13836f
HeemTempo <heemsterling1706@gmail.com>
2026-02-21 12:42:02 +0300

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,29 +1,7 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: '/api',
-//   graphqlUrl: '/graphql/',
-//   wsUrl: '/ws',
-//   mediaUrl: '/media',
-//   firebase: {
-//     apiKey: "AIzaSyD4d_WpT1X7d7UtvnPnCz4DSZhmg2O2LeQ",
-//     authDomain: "openspace-a2cff.firebaseapp.com",
-//     projectId: "openspace-a2cff",
-//     storageBucket: "openspace-a2cff.appspot.app",
-//     messagingSenderId: "644156117035",
-//     appId: "1:644156117035:web:31ef865a08fa9b2cb1da6e",
-//     measurementId: "G-YZYQ0SDSXF"
-//   }
-// };
-
-
-
-
-
-
 export const environment = {
   production: true,
   // Your live Dokploy Django URL
-  apiUrl: 'https://api.your-django-backend.com', 
+  apiUrl: 'http://95.111.247.129:8000', 
   // Your live GraphQL endpoint
-  graphqlUrl: 'https://api.your-django-backend.com' 
+  graphqlUrl: 'http://95.111.247.129:8000' 
 };
